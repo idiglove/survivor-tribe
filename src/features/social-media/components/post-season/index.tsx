f5ce44b433d2fe0914bb50345a80850b1bcbfcb9
Faith Morante <faith.morante@yocale.com>
2024-12-21 13:19:19 -0800

    initial commit

---
+++ src/features/social-media/components/post-season/index.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+import { Card, CardContent } from "@/shared/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/shared/components/ui/carousel";
+
+export function SocialMediaPostSeason() {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-sm mx-auto"
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-3xl bg-red-300 font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
