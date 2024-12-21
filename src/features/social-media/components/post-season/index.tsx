f7f8b92dec662ad962426292f1bf9f8328eb0361
Faith Morante <faith.morante@yocale.com>
2024-12-21 13:43:51 -0800

    add images from imagekit

--- src/features/social-media/components/post-season/index.tsx
+++ src/features/social-media/components/post-season/index.tsx
@@ -8,8 +8,12 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/shared/components/ui/carousel";
+import fetchImages from "../../api/fetch-images";
+import Image from "next/image";
 
-export function SocialMediaPostSeason() {
+export async function SocialMediaPostSeason() {
+  const images = await fetchImages();
+
   return (
     <Carousel
       opts={{
@@ -18,12 +22,16 @@ export function SocialMediaPostSeason() {
       className="w-full max-w-sm mx-auto"
     >
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
+        {images.map((image, index: number) => (
           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
             <div className="p-1">
               <Card>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl bg-red-300 font-semibold">{index + 1}</span>
+                  <Image
+                    src={image.url}
+                    alt="Survior Post Season 47"
+                    layout="fill"
+                  />
                 </CardContent>
               </Card>
             </div>
