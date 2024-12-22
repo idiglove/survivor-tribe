2026b3aa306f55ec33b44e3deea53914771501ac
Faith Morante <faith.morante@yocale.com>
2024-12-21 21:16:59 -0800

    add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import ComingSoon from "@/features/coming-soon/components";
 import { SocialMediaPostSeason } from "@/features/social-media/components/post-season";
 import HomepageHeader from "@/shared/components/homepage/header";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Survivor Tribe - Reality Game Show Fan Site",
-  description: "Get all updates about our favorite reality game show here!",
-};
 
 export default function Home() {
   return (
     <>
       <main>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu+Sans:ital,wght@0,100..800;1,100..800&display=swap"
+          rel="stylesheet"
+        />
         <HomepageHeader />
 
         <div className="w-full max-w-[900px] mx-auto">
@@ -26,7 +26,8 @@ export default function Home() {
             Reserved.
           </p>
           <p className="text-sm">
-            This is a Survivor fan site and is not affiliated with Survior or CBS.
+            This is a Survivor fan site and is not affiliated with Survior or
+            CBS.
           </p>
         </div>
       </footer>
