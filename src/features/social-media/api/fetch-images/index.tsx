1446388f9a7232adb7a9d02528bc9312d1f68799
Faith Morante <faith.morante@yocale.com>
2024-12-22 20:15:44 -0800

    show latest first on image gallery

--- src/features/social-media/api/fetch-images/index.tsx
+++ src/features/social-media/api/fetch-images/index.tsx
@@ -1,6 +1,6 @@
 const fetchImages = async () => {
   const response = await fetch(
-    "https://api.imagekit.io/v1/files?fileType=image&path=/post-season-47",
+    "https://api.imagekit.io/v1/files?fileType=image&path=/post-season-47&sort=DESC_CREATED",
     {
       headers: {
         Accept: "application/json",
