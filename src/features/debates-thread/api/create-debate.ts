9624da5e8941e3d74d7f0473a05df209ce284f64
Faith Morante <faith.morante@yocale.com>
2024-12-27 18:59:04 -0800

    post debate

--- src/features/debates-thread/api/create-debate.ts
+++ src/features/debates-thread/api/create-debate.ts
@@ -1,19 +1,19 @@
 "use server";
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
-import { connectToMongoDB } from "@/shared/lib/mongoose";
 import { generateUsername } from "unique-username-generator";
+import Debate from "@/shared/models/debate";
+import { connectToMongoDB } from "@/shared/lib/mongoose";
 import { DebateFormSchema } from "../components/form";
 
 export const createDebate = async (
   formData: z.infer<typeof DebateFormSchema>
 ) => {
-  const mongo = await connectToMongoDB();
-  const { models } = mongo ?? {};
+  await connectToMongoDB();
   const { post } = formData ?? {};
   const user = generateUsername("-", 0, 15);
   try {
-    const newDebate = await models.debates.create({
+    const newDebate = await Debate.create({
       post,
       user,
     });
