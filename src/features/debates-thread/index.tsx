8f4625f580c9979b2e6a6c497f1ec67c8547a9e7
Faith Morante <faith.morante@yocale.com>
2024-12-28 19:29:25 -0700

    fetch debate in client

--- src/features/debates-thread/index.tsx
+++ src/features/debates-thread/index.tsx
@@ -1,16 +1,24 @@
-import * as React from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { WATCH_PARAMOUNT } from "@/shared/constants";
 import { DebateForm } from "./components/form";
 import { getDebate } from "./api/get-debate";
 
-const DebatesThread = async () => {
-  const debates = await getDebate();
+const DebatesThread = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const debates = await getDebate();
+      setData(debates);
+    };
+    fetchData();
+  }, []);
   return (
     <div id={WATCH_PARAMOUNT} className="flex flex-col items-center px-8">
       <h2 className="w-full text-3xl font-bold text-center my-8">
         Debate about Survivor Theories here!
       </h2>
-      <DebateForm debates={debates} />
+      <DebateForm debates={data} />
     </div>
   );
 };
