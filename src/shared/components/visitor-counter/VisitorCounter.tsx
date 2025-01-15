01b488bd36997e41c36a1815882dcfbf1d9efc9d
iammannpreet <vickyarora.singh@gmail.com>
2025-01-15 14:16:12 -0500

    removed set error and data is being loaded dynamically

--- src/shared/components/visitor-counter/VisitorCounter.tsx
+++ src/shared/components/visitor-counter/VisitorCounter.tsx
@@ -5,29 +5,27 @@ import { getVisitorCount } from '../../../app/actions/getVisitorCount';
 
 const VisitorCounter = () => {
   const [visitorCount, setVisitorCount] = useState<number | null>(null);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchVisitorCount = async () => {
       try {
         const count = await getVisitorCount();
+        
         setVisitorCount(count);
       } catch (err) {
-        console.error('Error fetching visitor count:', err); // Log the error
-        setError('Failed to load visitor count. Please try again later.');
+        console.error('Error fetching visitor count:', err);
       }
     };
 
     fetchVisitorCount();
-  }, []);
 
-  if (error) {
-    return (
-      <div className="fixed top-0 left-0 w-full text-center bg-red-500 py-2 text-lg font-bold text-white z-50 shadow-md">
-        ⚠️ {error}
-      </div>
-    );
-  }
+    const interval = setInterval(() => {
+      console.log('Refreshing visitor count...');
+      fetchVisitorCount();
+    }, 30000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   if (visitorCount === null) {
     return (
