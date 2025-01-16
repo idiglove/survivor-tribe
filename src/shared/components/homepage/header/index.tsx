0cebc455689e9c931caa83f0659aad7646a0dffb
francesjgonzales <francesj7837@gmail.com>
2025-01-15 23:39:27 -0700

    modified outer wrapper

--- src/shared/components/homepage/header/index.tsx
+++ src/shared/components/homepage/header/index.tsx
@@ -1,26 +1,32 @@
 import Link from "next/link";
 import Image from "next/image";
-import Logo from "@/assets/survivor-tribe-logo.png";
-import Navbar from "../../navbar";
+import HeroImg from "@/assets/jeff_probst.png";
+/* import Navbar from "../../navbar"; */
 import { Button } from "../../ui/button";
+import TopNavBar from "../../top-sticky-navbar";
 
 const HomepageHeader = () => {
   return (
     <header className="text-center">
-      <Navbar />
-      <div className="w-full h-full min-h-[300px] flex flex-col items-center justify-center gap-5">
-        <Link href="/">
-          <Image src={Logo} alt="Survivor Tribe Logo" width={400} />
-        </Link>
+      <TopNavBar />
+      <div className="w-full h-full min-h-[300px] flex flex-col items-center justify-center gap-5 ">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="bg-[#402E321F]">
+            <p className="text-[#402E32] text-6xl font-bold">survivor tribe</p>
+            <p className="font-bold text-3xl text-white font-semibold">
+              Be a part of the ultimate Survivor Fan Site
+            </p>
+          </div>
+          <div>
+            <Image src={HeroImg} alt="Jeff Probst" width={400} />
+          </div>
+        </div>
+
         <Button variant="secondary">
           <a href="https://discord.gg/C5dTyfnt8R" target="__blank">
             Join our Discord Server!
           </a>
         </Button>
-        <h3 className="font-bold">
-          Get all updates about Survivor Season 47 and upcoming seasons!
-        </h3>
-        <h4>Be a part of the ultimate Survivor Fan Community</h4>
       </div>
     </header>
   );
